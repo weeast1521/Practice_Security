f251134ad59b86bd7967bd8927fe89ea7742d89e
donghyukseo <ehdgurdusdn@naver.com>
2025-04-10 22:00:21 +0900

    main.mustache 추가 및 MainController 추가

--- src/main/java/com/example/testsecurity/TestSecurityApplication.java
+++ src/main/java/com/example/testsecurity/TestSecurityApplication.java
@@ -1,4 +1,4 @@
-package com.example.TestSecurity;
+package com.example.testsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/testsecurity/TestSecurityApplicationTests.java
+++ src/test/java/com/example/testsecurity/TestSecurityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.TestSecurity;
+package com.example.testsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
