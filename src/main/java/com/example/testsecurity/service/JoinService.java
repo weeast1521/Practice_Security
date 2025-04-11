2250527635b9f11da7e596b769b82bfb3a99bd7c
donghyukseo <ehdgurdusdn@naver.com>
2025-04-11 16:18:04 +0900

    feat : username 중복을 막기 위한 기능 추가

--- src/main/java/com/example/testsecurity/service/JoinService.java
+++ src/main/java/com/example/testsecurity/service/JoinService.java
@@ -19,6 +19,11 @@ public class JoinService {
     // db에 이미 동일한 username을 가진 회원이 존재하는지?
     public void joinProcess(JoinDTO joinDTO) {
 
+        boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+        if (isUser) {
+            return;
+        }
+
         UserEntity data = new UserEntity();
 
         data.setUsername(joinDTO.getUsername());
