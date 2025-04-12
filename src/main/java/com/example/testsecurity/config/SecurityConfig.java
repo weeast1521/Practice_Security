893e9e2cce485d33be120f98c3e75f8dc5ba4330
donghyukseo <ehdgurdusdn@naver.com>
2025-04-13 01:43:33 +0900

    feat : HttpBasic 로그인 기능 구현

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.testsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -33,9 +34,10 @@ public class SecurityConfig {
                 );
 
         http
-                .formLogin((auth) -> auth.loginPage("/login")
-                        .loginProcessingUrl("/loginProc").permitAll()
-                );
+//                .formLogin((auth) -> auth.loginPage("/login")
+//                        .loginProcessingUrl("/loginProc").permitAll()
+//                );
+                .httpBasic(Customizer.withDefaults());
 
         http
                 .sessionManagement((auth) -> auth
